2ab1a5187c369b3e436b3270f02310e98ac5b29e
Alex Davis <alex@alex2006hw.com>
2016-04-04 17:38:28 -0700

    moving-to-react-native

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 -- WIP
 - v1.0.0 : https://github.com/ronik-design/react-drive-in
 - v1.1.0 : https://github.com/thereactivestack/kickstart-flowrouter
+- v1.2.0 : https://github.com/c9/core.git
+- v1.3.0 : react-native https://www.udemy.com/reactnative/learn/v4/t/lecture/3788996
 
 (c) 2015 PointLook
--- b9/lib/_npm.sh
+++
@@ -1,8 +0,0 @@
-_b9_npm() {
-    local WORKDIR=$1
-    shift
-    docker run --rm -w /home/ubuntu/newclient -v $WORKDIR:/home/ubuntu/newclient -v $HOME/.ssh:/home/ubuntu/.ssh:ro --sig-proxy -a STDIN -a STDOUT -a STDERR $(_b9_get_newclient_image) npm "$@"
-    # pushd $WORKDIR
-    # npm "$@"
-    # popd
-}
